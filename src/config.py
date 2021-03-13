4d1657d5e43d437bffd2770293581bb6420f365a
Filip <fdabek1@umbc.edu>
2021-03-13 01:48:22 -0500

    Cleaned up formatting.

--- src/config.py
+++ src/config.py
@@ -1,23 +1,21 @@
-class Config():
-    sequence_lengths = [7,9,6,3,4] #[10,12,6,20,20]
-    vector_sizes = [8,10,12,14,16,18] #[4,6,8,10] #letters,words,sentences,paragraphs,chapters,book
-    num_heads = [2,2,2,2,2,2]#[2,3,4,5] #for transformers
-    fnn_sizes = [8,10,12,14,16,18] #[2,3,4,5] #for fnn in transformers
-    vocab_sizes = [80,20,10,8,8,8]#[1000,21,6,5
-    num_transformer_layers = [2,2,2,2,2,2]#[2,2,2,2]
+class Config:
+    sequence_lengths = [7, 9, 6, 3, 4]  # [10,12,6,20,20]
+    vector_sizes = [8, 10, 12, 14, 16, 18]  # [4,6,8,10] #letters,words,sentences,paragraphs,chapters,book
+    num_heads = [2, 2, 2, 2, 2, 2]  # [2,3,4,5] #for transformers
+    fnn_sizes = [8, 10, 12, 14, 16, 18]  # [2,3,4,5] #for fnn in transformers
+    vocab_sizes = [80, 20, 10, 8, 8, 8]  # [1000,21,6,5
+    num_transformer_layers = [2, 2, 2, 2, 2, 2]  # [2,2,2,2]
     dtype = 'float32'
-    mlm_rate = 0.15 #0.15 like BERT
-    batch_size = 2 #todo: use it to create the actual dataset, it is also hardcoded there
+    mlm_rate = 0.15  # 0.15 like BERT
+    batch_size = 2  # todo: use it to create the actual dataset, it is also hardcoded there
     drop_rate = 0.15
     batch_size = batch_size
 
-    pad_token_id = 1 #hard coded; will break logic if changed!!!
-    eos_token_id = 2 #hard coded; will break logic if changed!!!
-    join_token_id = 3 #hard coded; will break logic if changed!!!
+    pad_token_id = 1  # hard coded; will break logic if changed!!!
+    eos_token_id = 2  # hard coded; will break logic if changed!!!
+    join_token_id = 3  # hard coded; will break logic if changed!!!
     join_texts = False
-    agent_level = 2 #most complex vector agent can create 2=paragraph
-
-
+    agent_level = 2  # most complex vector agent can create 2=paragraph
 
 # MODEL_CONFIG = [
 #     {
--- src/tree_dataset.py
+++ src/tree_dataset.py
@@ -2,12 +2,14 @@ from src.pre_processing import TreeTokenizer
 
 
 class TreeDataset:
-    def __init__(self, max_level=2):
+    def __init__(self):
         self.tree_tokenizer = TreeTokenizer()
-        self.texts = ["I like big butts. I can not lie.","some other song"] #hard coded for tests
+        self.texts = ["I like big butts. I can not lie.", "some other song"]  # hard coded for tests
 
     def iterator(self):
-        yield self.tree_tokenizer.batch_texts_to_trees(self.texts) #[self.tree_tokenizer.batch_texts_to_trees(item) for item in self.texts]
+        yield self.tree_tokenizer.batch_texts_to_trees(self.texts)
+
 
 if __name__ == '__main__':
     dataset = TreeDataset()
+    print(dataset.iterator())

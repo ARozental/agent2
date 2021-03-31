ac200df2372f28b1ab629c2d8e2fd528ac31f402
Filip <fdabek1@umbc.edu>
2021-03-31 18:07:58 -0400

    Easy check for dummy dataset.

--- src/datasets/dummy.py
+++ src/datasets/dummy.py
@@ -1,11 +1,13 @@
 from src.pre_processing import Splitters, TreeTokenizer
 from src.pre_processing.dataset import Dataset
+from src.config import Config
 import os
 
 
 class DummyDataset(Dataset):
     def __init__(self, **kwargs):
         super().__init__(folder=os.path.join('datasets', 'dummy', '*.txt'), **kwargs)
+        assert Config.agent_level <= Config.levels['PARAGRAPH']
 
     def init_tree_tokenizer(self):
         TreeTokenizer.split_functions = [

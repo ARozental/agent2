f31d9add6ffd09dd347aaf1a2fe79fb5b1d876a8
Filip Dabek <filip.dabek@amobee.com>
2021-04-14 03:09:00 +0300

    Experiment config

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import torch
 
 
 class Config:
-    sequence_lengths = [7, 9, 6, 3, 4]  # [10,12,6,20,20]
+    sequence_lengths = [16, 16, 6, 3, 4]  # [10,12,6,20,20]
     # vector_sizes = [8, 10, 12, 14, 16, 18]  # [4,6,8,10] #letters,words,sentences,paragraphs,chapters,book
     vector_sizes = [32, 48, 64, 96, 128, 156]  # [4,6,8,10] #letters,words,sentences,paragraphs,chapters,book
     num_heads = [2, 2, 2, 2, 2, 2]  # [2,3,4,5] #for transformers
@@ -10,13 +10,13 @@ class Config:
     num_transformer_layers = [2, 2, 2, 2, 2, 2]  # [2,2,2,2]
     mlm_rate = 0.15  # 0.15 like BERT
     batch_size = 2  # How many books/articles/etc per batch.
-    batch_sizes = [6, 1000, 1000, 1000, 1000]  # How many nodes to process at a time at each level
+    batch_sizes = [3000, 3000, 1000, 1000, 1000]  # How many nodes to process at a time at each level
     drop_rate = 0.0
 
     pad_token_id = 1  # hard coded; will break logic if changed!!!
     eos_token_id = 2  # hard coded; will break logic if changed!!!
     join_token_id = 3  # hard coded; will break logic if changed!!!
-    join_texts = False
+    join_texts = True
 
     # An easy way to remember the indices of each level
     levels = {

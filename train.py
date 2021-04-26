e35eca94d9a8d94dc2e13b1abdbcbf56c2400779
Alon Rozental <arozental@amobee.com>
2021-04-26 09:20:44 -0400

    sad panda

--- train.py
+++ train.py
@@ -66,7 +66,7 @@ def train():
             if Config.skip_batches is not None and (epoch == 0 and step < Config.skip_batches):
                 global_step += 1
                 continue
-            if step>50800 and step<50850: #todo: delete later
+            if step>50810 and step<50840: #todo: delete later
                 continue
 
             model.train()

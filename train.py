18be7fa1c7f279f5ec9258d9ffbd702ea9778ddf
Alon Rozental <arozental@amobee.com>
2021-04-29 21:49:11 -0400

    stuff for rcd

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ def train():
         main_optimizer = torch.optim.AdamW(main_params, Config.lr)
     else:
         main_optimizer = madgrad.MADGRAD(main_params, lr=Config.lr, momentum=Config.momentum)  # 0.01,0.9 is the default
-    main_optimizer = torch.optim.AdamW(main_params, 0.001) #todo: for dummy only
+    #main_optimizer = torch.optim.AdamW(main_params, 0.001) #todo: for dummy only
     generator_optimizer = torch.optim.AdamW(generator_params, 0.001)
     discriminator_optimizer = torch.optim.AdamW(discriminator_params, 0.001)
 

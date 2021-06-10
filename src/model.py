bde2aa9f7fc16c7fd6c6b2d48a12bf0e5ff8eef1
Alon Rozental <arozental@amobee.com>
2021-06-09 20:38:24 -0400

    fix

--- src/model.py
+++ src/model.py
@@ -197,8 +197,6 @@ class AgentModel(nn.Module):
                         for label, value in losses.items():
                             loss_object[level_num][label] += value.detach() / len(full_node_batch)
 
-                    with xp.Trace('ComputeTotalLoss' + str(level_num)):
-                      total_loss += main_loss.detach()
 
                     losses,main_loss,r_loss = None,None,None
 
@@ -229,7 +227,14 @@ class AgentModel(nn.Module):
                         node.rm_diff_loss = rm_diff_loss[i].detach()
                 mlm_loss, mlm_diff_loss, coherence_loss, reconstruction_loss, eos_loss, join_loss, reconstruction_diff_loss, re_loss, rm_loss = None, None, None, None, None, None, None, None, None
 
-
+        with xp.Trace('ComputeTotalLoss' + str(level_num)):
+          current_losses = []
+          for label, loss in loss_object[level_num].items():
+            if label not in ['g', 'disc', 'cd', 'rcd']:
+              current_losses.append(loss)
+              loss_object[level_num][label] = loss  # Keep loss_object as a tensor for custom backwards
+              # loss_object[level_num][label] = loss.item()  # Pull out of the GPU for logging
+          total_loss += torch.stack(current_losses).sum()
 
         return total_g_loss, total_disc_loss, total_loss, loss_object
 

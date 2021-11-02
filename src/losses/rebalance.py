f65f91035a6d15c1089ca8bf5ee8c861c55c78ed
Filip Dabek <fdabek1@umbc.edu>
2021-11-02 01:57:55 -0400

    Averaging losses before rebalance.

--- src/losses/rebalance.py
+++ src/losses/rebalance.py
@@ -28,6 +28,7 @@ class Rebalance:
 
     @classmethod
     def rebalance(cls):
+        cls.losses /= Config.rebalance_losses_aggregate
         for i in range(Config.agent_level + 1):
             level_weights = cls.get_level_weights(i)
             for name, value in level_weights.items():
@@ -37,7 +38,7 @@ class Rebalance:
 
     @classmethod
     def get_level_weights(cls, i):
-        loss_names = set(cls.losses[i].keys()) #maybe make sure none of them are 0 here as well?
+        loss_names = set(cls.losses[i].keys())  # Maybe make sure none of them are 0 here as well?
         num_empty_weights = len(loss_names - set(Config.rebalance_percentages[i].keys()))
         total_weights = [val for key, val in Config.rebalance_percentages[i].items() if val > 0 and key in loss_names]
         leftover_weight = 1.0 - sum(total_weights)

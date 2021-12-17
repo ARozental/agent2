0ff3fe28745650de361d32481697ff35e11f5756
Alon Rozental <arozental@amobee.com>
2021-12-17 10:32:22 -0500

    test

--- src/model.py
+++ src/model.py
@@ -35,7 +35,7 @@ class AgentModel(nn.Module):
         local_char_embedding_matrix = self.char_embedding_layer(local_char_embedding_tokens)
 
         # first encoder call
-        self.agent_levels[0].encoder.eval()
+        #self.agent_levels[0].encoder.eval()
         word_embedding_matrix = self.agent_levels[0].compressor(
             self.agent_levels[0].encoder(local_char_embedding_matrix, real_positions, eos_positions.float()),
             real_positions)  # [distinct_words_in_batch,word_vector_size]
